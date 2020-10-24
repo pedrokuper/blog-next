785a8c50c497ef004ea004085004f0447fd6d46f
Pedro <pkupersmit@gmail.com>
2020-10-23 21:45:48 -0300

    initial commit

---
+++ pages/articles/[id].js
@@ -0,0 +1,38 @@
+import ReactMarkdown from "react-markdown";
+
+export default function Articles(props) {
+  const { title, tags, cover_image, name, content } = props;
+  console.log(tags);
+  return (
+    <section>
+      <img src={cover_image} alt="" />
+      <h1>{title}</h1>
+      <ul>
+        {tags.map((tag, key) => {
+          return <li key={key}>{tag}</li>;
+        })}
+      </ul>
+      <p>{name}</p>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </section>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const { id } = params;
+  const res = await fetch(`https://dev.to/api/articles/${id}`);
+  const data = await res.json();
+  console.log(data.user.name);
+  const { cover_image, tags, title, body_markdown } = data;
+  const { name } = data.user;
+  return {
+    props: {
+      id,
+      name,
+      cover_image,
+      tags,
+      title,
+      content: body_markdown,
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <section className={styles.postsContainer}>
+        {articles.map((post, key) => {
+          return (
+            <>
+              <Link href={`/articles/${post.id}`}>
+                <a className={styles.posts} key={key}>
+                  <h3>{post.title}</h3>
+                  <h4>{post.description}</h4>
+                </a>
+              </Link>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export async function getStaticProps() {
+  const data = await fetch("https://dev.to/api/articles?tag=javascript&top=1");
+  const json = await data.json();
+  const mappedData = json.map((article) => {
+    return article;
+  });
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  return {
+    props: {
+      articles: mappedData,
+    },
+    revalidate: 3600,
+  };
 }
