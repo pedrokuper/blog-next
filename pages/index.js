43ca871abb6ad66534cde7f1c9837de7448c1137
Pedro <pkupersmit@gmail.com>
2020-10-24 04:13:25 -0300

    margins added in posts home page

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ articles }) {
                       <span>{post.readable_publish_date}</span>
                     </div>
                   </div>
-                  <a>
+                  <a className={styles.postTextContainer}>
                     <h3 className={styles.title}>{post.title}</h3>
                     {post.tag_list.map((tag) => {
                       return <span className={styles.tags}>#{tag}</span>;
@@ -54,7 +54,11 @@ export default function Home({ articles }) {
                         >
                           <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z"></path>
                         </svg>
-                        <span>{`${post.comments_count} comments`}</span>
+                        {post.comments_count < 1 ? (
+                          <span>Add a comment</span>
+                        ) : (
+                          <span>{`${post.comments_count} comments`}</span>
+                        )}
                       </div>
                     </div>
                   </a>
@@ -76,5 +80,6 @@ export async function getStaticProps() {
     props: {
       articles: json,
     },
+    revalidate: 3600,
   };
 }
